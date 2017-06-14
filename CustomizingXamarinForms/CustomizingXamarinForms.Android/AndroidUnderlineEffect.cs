91f7e49e1dbf84e7db88ae84b22d1924ece1dbf9
Rob Gibbens <RobGibbens@gmail.com>
2017-06-14 00:03:15 -0400

    Added Android UnderlineEffect

---
+++ CustomizingXamarinForms/CustomizingXamarinForms.Android/AndroidUnderlineEffect.cs
@@ -0,0 +1,32 @@
+﻿using Android.Graphics;
+using Android.Widget;
+using Xamarin.Forms.Platform.Android;
+
+namespace CustomizingXamarinForms.Droid
+{
+    public class AndroidUnderlineEffect : PlatformEffect
+    {
+        private void AddUnderline()
+        {
+            var textView = (TextView)Control;
+
+            textView.PaintFlags |= PaintFlags.UnderlineText;
+        }
+
+        private void RemoveUnderline()
+        {
+            var textView = (TextView)Control;
+            textView.PaintFlags &= ~PaintFlags.UnderlineText;
+        }
+
+        protected override void OnAttached()
+        {
+            AddUnderline();
+        }
+
+        protected override void OnDetached()
+        {
+            RemoveUnderline();
+        }
+    }
+}
