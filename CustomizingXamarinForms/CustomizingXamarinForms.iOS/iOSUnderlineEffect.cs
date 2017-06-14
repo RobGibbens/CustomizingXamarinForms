8ea91246363e32de3c5514dcb6ee6f8fb04c36a1
Rob Gibbens <RobGibbens@gmail.com>
2017-06-14 00:27:54 -0400

    Added iOSEffect

---
+++ CustomizingXamarinForms/CustomizingXamarinForms.iOS/iOSUnderlineEffect.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms.Platform.iOS;
+
+namespace CustomizingXamarinForms.iOS
+{
+    public class iOSUnderlineEffect : PlatformEffect
+    {
+        private void AddUnderline()
+        {
+            var label = (UILabel)Control;
+
+            if (label == null)
+            {
+                return;
+            }
+
+            var text = (NSMutableAttributedString)label.AttributedText;
+            var range = new NSRange(0, text.Length);
+
+            text.AddAttribute(UIStringAttributeKey.UnderlineStyle, NSNumber.FromInt32((int)NSUnderlineStyle.Single), range);
+        }
+
+        private void RemoveUnderline()
+        {
+            var label = (UILabel)Control;
+
+            if (label == null)
+            {
+                return;
+            }
+
+            var text = (NSMutableAttributedString)label.AttributedText;
+            var range = new NSRange(0, text.Length);
+
+            text.RemoveAttribute(UIStringAttributeKey.UnderlineStyle, range);
+        }
+
+        protected override void OnAttached()
+        {
+            AddUnderline();
+        }
+
+        protected override void OnDetached()
+        {
+            RemoveUnderline();
+        }
+    }
+}
