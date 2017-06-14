b7f23280859b9dccd3f5f4f1063e9d94937785bb
Rob Gibbens <RobGibbens@gmail.com>
2017-06-13 23:12:20 -0400

    Added Device.Idiom

--- CustomizingXamarinForms/CustomizingXamarinForms/MainPage.xaml.cs
+++ CustomizingXamarinForms/CustomizingXamarinForms/MainPage.xaml.cs
@@ -3,27 +3,52 @@ using Xamarin.Forms;
 
 namespace CustomizingXamarinForms
 {
-	public partial class MainPage : ContentPage
-	{
-	    private Helpers helpers;
-		public MainPage()
-		{
-			InitializeComponent();
+    public partial class MainPage : ContentPage
+    {
+        private Helpers helpers;
+        public MainPage()
+        {
+            InitializeComponent();
 
             helpers = new Helpers();
 
-		    switch (Device.RuntimePlatform)
-		    {
-		        case Device.iOS:
-		            registerLabel.Text = "Register this iOS device";
-		            break;
-		        case Device.Android:
-		            registerLabel.Text = "Register this Android tablet";
-		            break;
-		        case Device.Windows:
-		            registerLabel.Text = "Register this Windows device";
-		            break;
-		    }
+            switch (Device.RuntimePlatform)
+            {
+                case Device.iOS:
+                    switch (Device.Idiom)
+                    {
+                        case TargetIdiom.Phone:
+                            registerLabel.Text = "Register this iPhone";
+                            break;
+                        case TargetIdiom.Tablet:
+                            registerLabel.Text = "Register this iPad";
+                            break;
+                    }
+                    break;
+                case Device.Android:
+                    switch (Device.Idiom)
+                    {
+                        case TargetIdiom.Phone:
+                            registerLabel.Text = "Register this Android phone";
+                            break;
+                        case TargetIdiom.Tablet:
+                            registerLabel.Text = "Register this Android tablet";
+                            break;
+                    }
+
+                    break;
+                case Device.Windows:
+                    switch (Device.Idiom)
+                    {
+                        case TargetIdiom.Phone:
+                            registerLabel.Text = "Register this Windows Phone";
+                            break;
+                        case TargetIdiom.Desktop:
+                            registerLabel.Text = "Register this Windows desktop";
+                            break;
+                    }
+                    break;
+            }
         }
 
         async void SignDocument_Clicked(object sender, EventArgs args)
@@ -31,9 +56,9 @@ namespace CustomizingXamarinForms
             await Navigation.PushAsync(new SignaturePage());
         }
 
-	    private void Save_OnClicked(object sender, EventArgs e)
-	    {
-	        
-	    }
-	}
+        private void Save_OnClicked(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
