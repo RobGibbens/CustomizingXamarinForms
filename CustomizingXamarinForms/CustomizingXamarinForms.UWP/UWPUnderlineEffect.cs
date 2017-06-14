323ccb26ad36925224cb8144033b3d62de2e2347
Rob Gibbens <RobGibbens@gmail.com>
2017-06-14 00:38:43 -0400

    Added UWP Effect

---
+++ CustomizingXamarinForms/CustomizingXamarinForms.UWP/UWPUnderlineEffect.cs
@@ -0,0 +1,53 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Documents;
+using Xamarin.Forms.Platform.UWP;
+
+namespace CustomizingXamarinForms.UWP
+{
+    public class UWPUnderlineEffect : PlatformEffect
+    {
+        private void AddUnderline()
+        {
+            var label = (TextBlock)Control;
+
+            if (label == null)
+            {
+                return;
+            }
+
+            var originalText = label.Text;
+            Underline ul = new Underline();
+            Run run = new Run {Text = originalText};
+            ul.Inlines.Add(run);
+            label.Inlines.Clear();
+            label.Inlines.Add(ul);
+        }
+
+        private void RemoveUnderline()
+        {
+            var label = (TextBlock)Control;
+
+            if (label == null)
+            {
+                return;
+            }
+
+            var originalText = label.Text;
+            
+            Run run = new Run { Text = originalText };
+
+            label.Inlines.Clear();
+            label.Inlines.Add(run);
+        }
+
+        protected override void OnAttached()
+        {
+            AddUnderline();
+        }
+
+        protected override void OnDetached()
+        {
+            RemoveUnderline();
+        }
+    }
+}
