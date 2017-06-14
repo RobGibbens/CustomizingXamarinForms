36372c04ceee8dca2701e2a78d5404ba81281883
Rob Gibbens <RobGibbens@gmail.com>
2017-06-14 11:23:35 -0400

    Added ContentPage

---
+++ NativeProjects/NativeEmbedding/NativeEmbedding/NativeEmbedding/RegisteredDevicesPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace NativeEmbedding
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class RegisteredDevicesPage : ContentPage
+    {  
+        public RegisteredDevicesPage()
+        {
+            InitializeComponent();
+            
+            var viewModel = new RegisteredDevicesViewModel();
+
+            BindingContext = viewModel;
+        }
+
+        async void Handle_ItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            if (e.Item == null)
+                return;
+
+            var device = (RegisteredDevice) e.Item;
+            await DisplayAlert($"{device.UserName} selected", $"{device.Platform} {device.Version}", "OK");
+
+            ((ListView)sender).SelectedItem = null;
+        }
+    }
+}
