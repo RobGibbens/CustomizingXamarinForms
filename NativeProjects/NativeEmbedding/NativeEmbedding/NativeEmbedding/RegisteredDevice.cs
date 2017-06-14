b71869926d6be40c09d3596cbb15dfe5af8b5e6d
Rob Gibbens <RobGibbens@gmail.com>
2017-06-14 11:12:18 -0400

    Added ViewModel

---
+++ NativeProjects/NativeEmbedding/NativeEmbedding/NativeEmbedding/RegisteredDevice.cs
@@ -0,0 +1,9 @@
+﻿namespace NativeEmbedding
+{
+    public class RegisteredDevice
+    {
+        public string UserName { get; set; }
+        public string Platform { get; set; }
+        public string Version { get; set; }
+    }
+}
